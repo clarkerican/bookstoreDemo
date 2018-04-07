60aa8dadb441781137476a6338f88f7b3340be35
Erica Clark <enclark@iastate.edu>
2018-04-06 20:55:31 -0500

    Creates a template

--- src/index.js
+++ src/index.js
@@ -1,45 +1,16 @@
 const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
 const routes = require('./Routes/routes');
 
 const app = express();
 
-/* These are application-level middleware.  They run before the routes are computed. */
-app.use(morgan('dev')); // provides logging for each of the endpoints in the terminal
-app.use(bodyParser.json()); // nicely formats the request as a json object
-app.use(bodyParser.urlencoded({ extended: false })); // parses only UTF-8, url-encoded requests
-
-/* Routes */
+// routes
 app.use('/', routes);
 
-/* The request ends up here if it matches with no other routes, so it is a 404 Not Found error */
-app.use((req, res) => {
-  res.status(404).send({
-    status: 404,
-    message: 'Not Found',
-    type: 'NotFound'
-  });
-});
-
-/*
- * This handles any errors thrown but not handled by the routes. If the error already has a status
- * code, use that, otherwise, it will be a 500, Internal Server Error. Error handling middleware
- */
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: err
-  });
-});
-
-/* Set the port to 8080 */
+// set our port
 const port = process.env.PORT || 8080;
 
-/* Start app at specified port number */
+// start app at localhost:8080
 app.listen(port);
 
 console.log(`Listening on ${port}`);
-
 module.exports = app;
